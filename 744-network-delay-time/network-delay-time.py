9544a2039bc4d37428ca26a2e7e58586a1c5566e
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-07-25 18:50:36 -0400

    Time: 2032 ms (5.03%) | Memory: 20.5 MB (6.81%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,27 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        dist_tracker = [float("inf")]*(n+1)
+        dist_tracker[k] = 0
+        adj_List = [[] for _ in range(n+1)]
+        for a,b,w in times:
+            adj_List[a].append((b,w))
+        visited = [False]*(n+1)
+        def dfs(node,val):
+            print(node,val)
+            if val>dist_tracker[node]:
+                return
+            dist_tracker[node] = val
+            for neigbour,traveltime in adj_List[node]:
+                if traveltime+val < dist_tracker[neigbour]:
+                    dfs(neigbour,traveltime+val)
+
+        dfs(k,0)
+        maxi = -1
+        for i in range(1,len(dist_tracker)):
+            maxi = max(maxi,dist_tracker[i])
+        if maxi == float("inf"):
+            return -1
+        return maxi
+
+
+        
