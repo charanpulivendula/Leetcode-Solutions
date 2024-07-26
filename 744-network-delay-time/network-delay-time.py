cbca2cce61eba984899cfebadfec6def37522625
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-07-25 21:21:29 -0400

    Time: 1705 ms (5.03%) | Memory: 20.6 MB (6.81%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,26 +1,22 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        dist_tracker = [float("inf")]*(n+1)
-        dist_tracker[k] = 0
-        adj_List = [[] for _ in range(n+1)]
-        for a,b,w in times:
-            adj_List[a].append((b,w))
-        def dfs(node,val):
-            print(node,val)
-            if val>dist_tracker[node]:
-                return
-            dist_tracker[node] = val
-            for neigbour,traveltime in adj_List[node]:
-                if traveltime+val < dist_tracker[neigbour]:
-                    dfs(neigbour,traveltime+val)
+  def networkDelayTime(self, times, n, k):
+    adj = [[] for _ in range(n+1)]
+    distance = [float("inf")]*(n+1)
+    for s,d,w in times:
+      adj[s].append((d,w))
+      
+    def dfs(node,val):
+      if val >= distance[node]:
+        return
+      distance[node] = val
+      for neighbour,traveltime in adj[node]:
+        if val+traveltime < distance[neighbour]:
+          dfs(neighbour,val+traveltime)
+    
+    dfs(k,0)
+    print(distance)
+    for i in range(1,n+1):
+      if distance[i] == float('inf'):
+        return -1
 
-        dfs(k,0)
-        maxi = -1
-        for i in range(1,len(dist_tracker)):
-            maxi = max(maxi,dist_tracker[i])
-        if maxi == float("inf"):
-            return -1
-        return maxi
-
-
-        
+    return max(distance[1:])
