73a3edb227967b0faab6f6b967589c362423f7e1
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-08-08 21:51:30 -0400

    Time: 55 ms (5.02%) | Memory: 21.7 MB (12.14%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,20 +1,24 @@
 class Solution:
   def generateParenthesis(self, n):
     output = []
+    hash = set()
     def backtrack(string,res):
-      if len(string)>2*n:
-        return
-      print(res)
-      if res<0:
-        return
-      if len(string) == 2*n and res == 0:
-        output.append(string)
-      tempstring = string+"("
-      backtrack(tempstring,res+1)
-      tempstring = string + ")"
-      if res>0:
-        backtrack(tempstring,res-1)
-      return res
+        if string in hash:
+            return
+        if len(string)>2*n:
+            return
+        print(res)
+        if res<0:
+            return
+        hash.add(string)
+        if len(string) == 2*n and res == 0:
+            output.append(string)
+        tempstring = string+"("
+        backtrack(tempstring,res+1)
+        tempstring = string + ")"
+        if res>0:
+            backtrack(tempstring,res-1)
+        return res
 
     backtrack("",0)
     return output
