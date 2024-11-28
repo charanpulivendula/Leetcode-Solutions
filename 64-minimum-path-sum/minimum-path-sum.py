88815f07bc12a2b4c8e41ce39d34765600f22df5
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-11-28 01:13:15 -0500

    Time: 16 ms (46.81%) | Memory: 20.2 MB (14.80%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,19 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [[float('inf') for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = grid[0][0]
-        def checker(i,j):
-            return 0<=i<len(grid) and 0<=j<len(grid[0])
+        for i in range(1,m):
+            dp[i][0] = grid[i][0]+dp[i-1][0]
+        for j in range(1,n):
+            dp[0][j] = grid[0][j]+dp[0][j-1]
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if checker(i-1,j):
-                    dp[i][j] = min(dp[i][j],dp[i-1][j]+grid[i][j])
-                if checker(i,j-1):
-                    dp[i][j] = min(dp[i][j],dp[i][j-1]+grid[i][j])
-
-        return dp[-1][-1]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = grid[i][j]+min(dp[i-1][j],dp[i][j-1])
         
-        
-
-        
+        return dp[m-1][n-1]
