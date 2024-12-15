52961ec6e7d52eceed934e44eef4fe10223b25ea
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-12-14 22:47:36 -0500

    Time: 0 ms (100.00%) | Memory: 18.7 MB (7.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,35 +3,32 @@ class Solution:
         index = -1
         l = 0
         r = len(nums)-1
-        def search(l,r):
-            if l>r:
-                return [-1,-1]
-
-            if nums[l] == target and nums[r] == target:
-                return [l,r]
-
-            mid = (l+r)//2
-
-            if nums[mid]==target:
-                if nums[l]!=target:
-                    return search(l+1,r)
-                else:
-                    return search(l,r-1)
-
-            if nums[mid]<target:
-                return search(mid+1,r)
-                
-            elif nums[mid]>target:
-                return search(l,mid-1)
-
+        left = -1
+        right = -1
         while(l<=r):
             mid = (l+r)//2
             if nums[mid] == target:
-                return search(l,r)
+                left = mid
+                r = mid - 1 
 
             elif nums[mid]>target:
                 r = mid-1
 
             else:
                 l = mid+1
-        return [-1,-1]
+
+        l = 0
+        r = len(nums)-1
+        while(l<=r):
+            mid = (l+r)//2
+            if nums[mid] == target:
+                right = mid
+                l = mid + 1
+
+            elif nums[mid]>target:
+                r = mid-1
+
+            else:
+                l = mid+1
+
+        return [left,right]
