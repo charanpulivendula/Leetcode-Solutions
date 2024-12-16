daa4f9eeaa75882754952cf0679d3d9d7a5a1f63
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-12-15 22:13:53 -0500

    Time: 29 ms (37.93%) | Memory: 24.5 MB (37.26%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums):
         n = len(nums)
-        lprod = [1,nums[0]]
-        laccum = nums[0]
-        rprod = [1 for _ in range(n+1)]
-        rprod[-2] = nums[-1]
-        raccum = nums[-1]
-        for i in range(1,n):
-            lprod.append(nums[i]*laccum)
-            laccum = nums[i]*laccum
-
+        right = [1 for _ in range(n)]
+        right[-1] = nums[-1]
         for i in range(n-2,-1,-1):
-            rprod[i] = raccum*nums[i]
-            raccum*=nums[i]
+            right[i] = right[i+1]*nums[i]
+        right.append(1)
         res = []
-        # print(lprod)
-        # print(rprod)
-        for i in range(1,n+1):
-            res.append(lprod[i-1]*rprod[i])
-        
+        # print(right)
+        left = 1
+        for i in range(n):
+            res.append(left*right[i+1])
+            left*=nums[i]
         return res
+
