72e00e33c42bb094687c58f7b609e63096048fea
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-12-19 10:24:53 -0800

    Time: 10 ms (41.52%) | Memory: 18.2 MB (25.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        valid = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        valid = "0123456789abcdefghijklmnopqrstuvwxyz"
         i = 0
         j = len(s)-1
         while(i<j):
             
-            if s[i] not in valid:
+            if s[i].lower() not in valid:
                 i+=1
                 continue
 
-            if s[j] not in valid:
+            if s[j].lower() not in valid:
                 j-=1
                 continue
 
