5dd6c525bd7f4bd4662003ec0571971d4d6a6a20
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2024-12-21 20:50:12 -0800

    Time: 207 ms (13.24%) | Memory: 49.3 MB (96.46%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,21 +1,27 @@
 from heapq import heappush,heappop
-
+# class point:
+#     def __init__(self,first,second):
+#         self.first = first
+#         self.second = second
+#     def __lt__(p1,p2):
+#         return p1.first<p2.first
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        heap = []
-        for point in points:
-            heappush(heap,point)
-        count = 0
-        while(len(heap)>1):
-            first = heappop(heap)
-            second = heappop(heap)
-            left = max(first[0],second[0])
-            right = min(first[1],second[1])
-            if left <= right:
-                heappush(heap,[left,right])
+        points.sort()
+        # print(points)
+        i = 1
+        count = 1
+        while(i<len(points)):
+            left = max(points[i-1][0],points[i][0])
+            right = min(points[i-1][1],points[i][1])
+            if left<=right:
+                points [i] = [left,right]
             else:
                 count+=1
-                heappush(heap,second)
-        return count+1
+            i+=1
+
+        return count      
+
+        
         
         
