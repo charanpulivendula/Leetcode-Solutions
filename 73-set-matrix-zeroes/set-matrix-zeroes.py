3c7f6f2a740d5ed1a659b350cd0e7117f64f11c2
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2025-04-21 11:49:04 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (95.82%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,9 +1,22 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        is_col=False
-        for i in range(len(matrix)):
+        flag=False
+        m = len(matrix)
+        n = len(matrix[0])
+        isrow = False
+        iscol = False
+        for i in range(m):
             if matrix[i][0]==0:
-                is_col=True
+                isrow = True
+                break
+
+        for i in range(n):
+            if matrix[0][i]==0:
+                iscol = True
+                break
+
+
+        for i in range(1,len(matrix)):
             for j in range(1,len(matrix[0])):
                 if matrix[i][j]==0:
                     matrix[i][0]=0
@@ -14,14 +27,15 @@ class Solution:
                 if not matrix[i][0] or not matrix[0][j]:
                     matrix[i][j] = 0
 
-        if matrix[0][0] == 0:
+
+        # see if the first column needs to be set to zero as well        
+        if isrow:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if iscol:
             for j in range(len(matrix[0])):
                 matrix[0][j] = 0
 
-        # see if the first column needs to be set to zero as well        
-        if is_col:
-            for i in range(len(matrix)):
-                matrix[i][0] = 0
         return matrix
         
         
