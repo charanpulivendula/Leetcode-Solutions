59fea7e30df23af9912005b0b96fc563dcb4ebf0
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2025-05-17 11:39:56 -0400

    Time: 144 ms (27.80%) | Memory: 18.4 MB (34.82%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,45 +1,28 @@
 from collections import defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        def equal(dict1,dict2):
-            if len(dict1)!=len(dict2):
-                return False
-            for char in dict1:
-                if dict1[char]!=dict2[char]:
-                    return False
-            return True
-
-        if len(s)<len(t):
-            return ""
-        if s==t:
-            return s
-        cache = defaultdict(int)
-        cur_dict = defaultdict(int)
-        for char in t:
-            cache[char]+=1
+        s_dict =defaultdict(int)
+        t_dict =  defaultdict(int)
         formed = 0
-        i = 0
-        j = 0
-        minlength = float('inf')
-        required = len(cache)
-        minstr = ""
-        while(i<len(s) and j<len(s)):
-            if s[j] in cache:
-                cur_dict[s[j]]+=1
-                if cur_dict[s[j]]==cache[s[j]]:
-                    formed+=1
-            # print(cur_dict)
-            while(i<=j and formed == required):
-                if j-i+1 < minlength:
-                    minlength = j-i+1
-                    minstr = s[i:j+1]
-                if s[i] in cur_dict:
-                    cur_dict[s[i]]-=1
-                    if cur_dict[s[i]]<cache[s[i]]:
-                        formed-=1
-                i+=1
-            j+=1
+        res = ""
+        minleng =  float('inf')
+        start = 0
+        for elem in t:
+            t_dict[elem]+=1
+        req = len(t_dict)
+        for end in range(len(s)):
+            s_dict[s[end]]+=1
+            if s[end] in t_dict and  s_dict[s[end]]==t_dict[s[end]]:
+                formed+=1
+            while formed == req:
+                leng = end-start+1
+                if leng<minleng:
+                    minleng = min(leng,minleng)
+                    res = s[start:end+1]
+                if s[start] in t_dict and s_dict[s[start]]==t_dict[s[start]]:
+                    formed-=1
+                s_dict[s[start]]-=1
+                start+=1
 
-
-        return minstr
+        return res
             
