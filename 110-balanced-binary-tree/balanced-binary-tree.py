f353d32b2e00170680b7657f360e25fd5dbbcbcf
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2025-05-24 16:23:22 -0400

    Time: 17 ms (6.22%) | Memory: 19.3 MB (7.70%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -1,31 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from collections import defaultdict
 class Solution:
-    def __init__(self):
-        self.cache = defaultdict(TreeNode)
-
+    def height(self,root):
+        if not root:
+            return 0
+        return 1+max(self.height(root.left),self.height(root.right))
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        if root in self.cache:
-            return self.cache[root]
-        def height(root):
-            if not root:
-                return 0
-            return 1+max(height(root.left),height(root.right))
-
-        leftheight = height(root.left)
-        rightheight = height(root.right)
-
-        self.cache[root] =  abs(leftheight-rightheight)<=1 and self.isBalanced(root.left) and self.isBalanced(root.right)
-        return self.cache[root]
-
-            
-        
-        
-        
+        leftheight = self.height(root.left)
+        rightheight = self.height(root.right)
+        print(leftheight,rightheight)
+        if abs(leftheight-rightheight)<=1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        else:
+            return False
