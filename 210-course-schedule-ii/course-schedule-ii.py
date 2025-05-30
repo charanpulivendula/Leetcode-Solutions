260930e6e361dc4986fedc358fcc8b041c337268
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2025-05-30 00:13:50 -0400

    Time: 7 ms (28.09%) | Memory: 19 MB (71.76%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -4,23 +4,25 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         res = []
         heap = []
-        indegree = defaultdict(set)
+        indegree = [0]*numCourses
+        graph = defaultdict(list)
         for a,b in prerequisites:
-            indegree[b].add(a)
-        print(indegree)
+            indegree[b]+=1
+            graph[a].append(b)
+        
         for node in range(numCourses):
-            if node not in indegree:
-                heappush(heap,node)
+            if indegree[node]==0:
+                heappush(heap, node)
 
         visited = set()
         while(heap):
             node = heappop(heap)
             res = [node]+res
             visited.add(node)
-            for neighbor in indegree:
-                if neighbor not in visited and node in indegree[neighbor]:
-                    indegree[neighbor].remove(node)
-                    if not indegree[neighbor]:
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    indegree[neighbor]-=1
+                    if indegree[neighbor]==0:
                         heappush(heap,neighbor)
 
         if len(res)==numCourses:
