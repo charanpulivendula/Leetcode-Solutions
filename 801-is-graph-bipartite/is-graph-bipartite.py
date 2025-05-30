edfb64022aa7e728b1fb151c77776ac4900ae3aa
Reddy Charan Pulivendula <115377126+charanpulivendula@users.noreply.github.com>
2025-05-29 23:38:22 -0400

    Time: 7 ms (15.68%) | Memory: 18.8 MB (15.18%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,20 +1,27 @@
+from collections import defaultdict
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        queue = deque()
-        visited = dict()
-        for start in range(len(graph)):
-            if start not in visited:
-                queue.append(start)
-                visited[start] = 1
-                while(queue):
-                    node = queue.popleft()
-                    for neighbor in graph[node]:
-                        if neighbor not in visited:
-                            queue.append(neighbor)
-                            visited[neighbor] = -1*visited[node]
-                        else:
-                            if visited[neighbor] == visited[node]:
-                                return False
+        
+        visited = defaultdict(int)
+
+        def dfs(node):
+
+            for neighbor in graph[node]:
+                if neighbor in visited:
+                    if visited[neighbor]==visited[node]:
+                        return False
+                    
+                else:
+                    visited[neighbor] = -1*visited[node]
+                    if not dfs(neighbor):
+                        return False
+
+            return True
+        for node in range(len(graph)):
+            if node not in visited:
+                visited[node] = 1
+                if not dfs(node):
+                    return False
         return True
 
 
